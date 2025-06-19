99099853b4ebfb2c1bfb85bd2691abadc2b4dc8c
JaimeClever <jaimecastillo.140920@gmail.com>
2025-06-19 18:03:51 -0500

    implementacion de UsuarioRepository

--- src/main/java/com/sistemaVenta/Grupo06/repository/UsuarioRepository.java
+++ src/main/java/com/sistemaVenta/Grupo06/repository/UsuarioRepository.java
@@ -1,4 +1,13 @@
 package com.sistemaVenta.Grupo06.repository;
 
-public interface UsuarioRepository {
+import com.sistemaVenta.Grupo06.entity.Rol;
+import com.sistemaVenta.Grupo06.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByUsername(String username);
+    boolean existsByUsername(String username);
+    boolean existsByRoles_Nombre(Rol.NombreRol nombre);
 }
