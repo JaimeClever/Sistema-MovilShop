2978f964c524289d39d17e46d35cd589a56c34ba
juanF <juanflores@gmail.com>
2025-06-19 19:13:07 -0500

    Subiendo Los metodos y atributos de Cliente en controller,service y de producto el Controller

--- src/main/java/com/sistemaVenta/Grupo06/service/ClienteService.java
+++ src/main/java/com/sistemaVenta/Grupo06/service/ClienteService.java
@@ -1,5 +1,44 @@
 package com.sistemaVenta.Grupo06.service;
 
-public class ClienteService {
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sistemaVenta.Grupo06.entity.Cliente;
+import com.sistemaVenta.Grupo06.repository.ClienteRepository;
+
+@Service
+
+public class ClienteService {
+	@Autowired
+    private ClienteRepository clienteRepository;
+
+    public Cliente guardar(Cliente cliente) {
+        return clienteRepository.save(cliente);
+    }
+
+    public List<Cliente> listarTodos() {
+        return clienteRepository.findAll();
+    }
+
+    public Optional<Cliente> buscarPorId(Long id) {
+        return clienteRepository.findById(id);
+    }
+
+    public Cliente actualizar(Long id, Cliente clienteActualizado) {
+        Cliente existente = clienteRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Cliente no encontrado"));
+
+        existente.setNombres(clienteActualizado.getNombres());
+        existente.setCorreo(clienteActualizado.getCorreo());
+        existente.setDni(clienteActualizado.getDni());
+
+        return clienteRepository.save(existente);
+    }
+
+    public void eliminar(Long id) {
+        clienteRepository.deleteById(id);
+}
 }
