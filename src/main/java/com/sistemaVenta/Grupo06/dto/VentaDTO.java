f10df770ced197f38f05db55be27ca64e01f369c
carlos <you@example.com>
2025-06-19 23:42:26 -0500

    acabo de agregar  la ventaDTO  y VentaResponseDTO

---
+++ src/main/java/com/sistemaVenta/Grupo06/dto/VentaDTO.java
@@ -0,0 +1,46 @@
+package com.sistemaVenta.Grupo06.dto;
+
+import java.util.List;
+
+
+public class VentaDTO {
+    private Long clienteId;
+    private List<ItemVentaDTO> items;
+
+    public static class ItemVentaDTO {
+        private Long productoId;
+        private Integer cantidad;
+
+        public Long getProductoId() {
+            return productoId;
+        }
+
+        public void setProductoId(Long productoId) {
+            this.productoId = productoId;
+        }
+
+        public Integer getCantidad() {
+            return cantidad;
+        }
+
+        public void setCantidad(Integer cantidad) {
+            this.cantidad = cantidad;
+        }
+    }
+
+    public Long getClienteId() {
+        return clienteId;
+    }
+
+    public void setClienteId(Long clienteId) {
+        this.clienteId = clienteId;
+    }
+
+    public List<ItemVentaDTO> getItems() {
+        return items;
+    }
+
+    public void setItems(List<ItemVentaDTO> items) {
+        this.items = items;
+    }
+}
