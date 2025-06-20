74085cebfc86dd9a332d1e4482c9adadf14bc3cc
juanF <juanflores@gmail.com>
2025-06-19 23:32:11 -0500

    corrección de errores

--- src/main/java/com/sistemaVenta/Grupo06/entity/DetalleVenta.java
+++ src/main/java/com/sistemaVenta/Grupo06/entity/DetalleVenta.java
@@ -1,9 +1,6 @@
 package com.sistemaVenta.Grupo06.entity;
 
-<<<<<<< HEAD
 
-=======
->>>>>>> develop
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -11,31 +8,25 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-<<<<<<< HEAD
+
 
 @Entity
 @Table(name = "detalle_venta")
 public class DetalleVenta {
 
     @Id
-=======
 
-@Entity
-@Table(name = "detalle_venta")
-public class DetalleVenta {
-	@Id
->>>>>>> develop
+
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private Integer cantidad;
     private Double precio;
-<<<<<<< HEAD
+
     private Double subtotal; // ✅ Agregar este campo
 
-=======
-    private Double subtotal; 
->>>>>>> develop
+
+
     @ManyToOne
     @JoinColumn(name = "producto_id")
     private Producto producto;
@@ -44,11 +35,9 @@ public class DetalleVenta {
     @JoinColumn(name = "venta_id")
     private Venta venta;
 
-<<<<<<< HEAD
+
     // Getters y Setters
-=======
-   
->>>>>>> develop
+
 
     public Long getId() {
         return id;
--- src/main/java/com/sistemaVenta/Grupo06/entity/Venta.java
+++ src/main/java/com/sistemaVenta/Grupo06/entity/Venta.java
@@ -3,11 +3,7 @@ package com.sistemaVenta.Grupo06.entity;
 import java.time.LocalDate;
 import java.util.List;
 
-<<<<<<< HEAD
 
-
-=======
->>>>>>> develop
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -25,15 +21,10 @@ public class Venta {
 	    @GeneratedValue(strategy = GenerationType.IDENTITY)
 	    private Long id;
 
-<<<<<<< HEAD
+
 	    private LocalDate fecha;
-=======
-	 @Id
-	    @GeneratedValue(strategy = GenerationType.IDENTITY)
-	    private Long id;
-        
-	 private LocalDate fecha;
->>>>>>> develop
+
+	 
 	    private Double total;
 
 	    @ManyToOne
@@ -106,10 +97,4 @@ public class Venta {
 	    public void setDetalles(List<DetalleVenta> detalles) {
 	        this.detalles = detalles;
 	    }
-<<<<<<< HEAD
 }
-=======
-	}
-
-
->>>>>>> develop
