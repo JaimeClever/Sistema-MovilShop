2a00daeb9c5b1fd2a6b32d6ec846461c580168db
carlos <you@example.com>
2025-06-19 21:28:10 -0500

    hice el detalle venta en el repository

---
+++ src/main/java/com/sistemaVenta/Grupo06/repository/DetalleVentaRepository.java
@@ -0,0 +1,14 @@
+package com.sistemaVenta.Grupo06.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.sistemaVenta.Grupo06.entity.DetalleVenta;
+
+
+
+@Repository
+
+public interface DetalleVentaRepository extends JpaRepository<DetalleVenta, Long> {
+
+}
